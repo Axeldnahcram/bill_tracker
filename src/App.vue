806c7174896221a8c52441fc25d94b10acd64193
Axel MARCHAND <amarchand@Axels-MacBook-Pro.local>
2019-05-21 17:32:24 +0200

    AXEL -UPD- stores category

--- src/App.vue
+++ src/App.vue
@@ -41,6 +41,20 @@ export default {
       this.categories.push(category)
       this.shouldShowAddCategory = false
     }
+  },
+  watch: {
+    categories() {
+      localStorage.setItem('categories', JSON.stringify(this.categories))
+    }
+  },
+  mounted() {
+    if (localStorage.getItem('categories')) {
+      this.categories = JSON.parse(localStorage.getItem('categories'))
+    }
+
+    if (!this.categories.length) {
+      this.shouldShowAddCategory = true
+    }
   }
 };
 </script>
