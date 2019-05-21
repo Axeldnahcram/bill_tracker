b278404d53f7360f89d53345eb608b6b2747f06c
Axel MARCHAND <amarchand@Axels-MacBook-Pro.local>
2019-05-21 17:44:20 +0200

    AXEL -UPD- added the add bill '

---
+++ src/components/AddBill.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Enter a new bill</h1>
+        <p></p>
+        <div>
+            <input placeholder="Set amount" v-model="amount">
+            <button @click="handleClick">Add</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AddBill',
+    data: function() {
+        return {
+            amount:0
+        }
+    },
+    methods: {
+        handleClick: function() {
+            //we will do it later
+        }
+    }
+}
+</script>
