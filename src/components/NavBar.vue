ad8c5ca02ca6090c21aee10f233b0ffc9c3275f7
Axel MARCHAND <amarchand@Axels-MacBook-Pro.local>
2019-05-21 17:40:51 +0200

    AXEL -UPD- some css added the plus button

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,14 @@
 <template>
-    <ul>
-        <li
-            v-for="category in categories"
-            :key="category">
-            {{category}}
-        </li>
-        <li @click="triggerShowAddCategory">+</li>
-    </ul>
+  <ul class="list-reset inline flex justify-center border-b-4 mb-0">
+    <li
+      class="p-4 inline bg-grey-lighter hover:bg-grey-light uppercase font-black cursor-pointer"
+      v-for="category in categories"
+      :key="category" >
+      {{category}}
+    </li>
+
+    <li class="p-4 inline bg-grey-lighter hover:bg-grey-light uppercase font-black cursor-pointer" @click="triggerShowAddCategory">➕</li>
+  </ul>
 </template>
 
 <script>
