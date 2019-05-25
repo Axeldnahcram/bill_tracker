d2ffb1e3c2319d3475aa73a022710dd2c1cccaec
Axel MARCHAND <amarchand@Axels-MacBook-Pro.local>
2019-05-25 11:36:28 +0200

    AXEL -UPD- first version finished

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,20 @@
 <template>
   <ul class="list-reset inline flex justify-center border-b-4 mb-0">
     <li
-      class="p-4 inline bg-grey-lighter hover:bg-grey-light uppercase font-black cursor-pointer"
+      class="p-4 inline hover:bg-grey-light uppercase font-black cursor-pointer"
+      :class="[ activeCategory === '' ? 'bg-grey-dark' : 'bg-grey-lighter' ]"
+      @click="clearActiveCategory()">
+      All
+    </li>
+    <li
+      class="p-4 inline hover:bg-grey-light uppercase font-black cursor-pointer"
+      :class="[ activeCategory === category ? 'bg-grey-dark' : 'bg-grey-lighter' ]"
       v-for="category in categories"
-      :key="category" >
+      :key="category"
+      @click="setActiveCategory(category)" >
       {{category}}
     </li>
+
 
     <li class="p-4 inline bg-grey-lighter hover:bg-grey-light uppercase font-black cursor-pointer" @click="triggerShowAddCategory">➕</li>
   </ul>
@@ -13,10 +22,16 @@
 
 <script>
 export default {
-    props: ['categories'],
+    props: ['categories', 'activeCategory'],
     methods: {
         triggerShowAddCategory() {
             this.$emit('triggerShowAddCategory')
+        },
+        clearActiveCategory() {
+          this.$emit('clearActiveCategory')
+        },
+        setActiveCategory(category) {
+          this.$emit('setActiveCategory', category)
         }
     }
 }
